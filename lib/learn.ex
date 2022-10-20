3c1f6fa9463c5aa781e5e14a21840c1f363330db
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-10-20 20:23:34 +0300

    done with collapse

--- lib/learn.ex
+++ lib/learn.ex
@@ -1,6 +1,6 @@
 defmodule Learn do
   def main do
-    expand("1")
+    collapse([1,2,4,5,6,8,9,12])
   end
 
   def expand(intervals) do
@@ -21,12 +21,51 @@ defmodule Learn do
       List.flatten(intervals_list)
     end
   end
+
+  def collapse(items) do
+    if items == [] do
+      ""
+    else
+      chunk_fun = fn
+        elem, [] -> {:cont, [elem]}
+        elem, [prev | _] = acc when prev + 1 == elem -> {:cont, [elem | acc]}
+        elem, acc -> {:cont, Enum.reverse(acc), [elem]}
+      end
+      after_fun = fn
+       [] -> {:cont, []}
+       acc -> {:cont, Enum.reverse(acc), []}
+      end
+      items = Enum.chunk_while(items, [], chunk_fun, after_fun)
+      items = Enum.map(items, fn item ->
+        if length(item) == 1 do
+          "#{List.last(item)}"
+        else
+          "#{List.first(item)}-#{List.last(item)}"
+        end
+      end)
+      items = Enum.join(items, ",")
+      items
+    end
+  end
+
+  # def union(intervals_1, intervals_2) do
+  #   ""
+  # end
+
+  # def intersection(intervals_1, intervals_2) do
+  #   ""
+  # end
+
+  # def difference(intervals_1, intervals_2) do
+  #   ""
+  # end
+
+  # def is_disjoint(intervals_1, intervals_2) do
+  #   true
+  # end
+
+  # def is_subset(intervals_1, intervals_2) do
+  #   true
+  # end
+
 end
-
-
-#Intervals: Expected result:
-#“”                       (empty string) [] (empty list)
-#“42”                     [42]
-#“4-5”                    [4, 5]
-#“4-6,10-12,16”           [4, 5, 6, 10, 11, 12, 16]
-#“1,3-5,12-14,9999”       [1, 3, 4, 5, 12, 13, 14, 9999]
